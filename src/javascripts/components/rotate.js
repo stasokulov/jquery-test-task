a3f0f999fab113e30db04f920d2d5e88b6e32a32
Stanislav Okulov <stasokulov@gmail.com>
2020-09-24 15:42:08 +0300

    заливаю файлы

--- src/javascripts/components/rotate.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-$.fn.rotatePlugin = function (data) {
-    const $targetThis = $(this);
-    let rotate = 0;
-    $(window).scroll(function () {
-        rotate = rotate + 5;
-        $targetThis.css({transform: 'rotate('+rotate+'deg)'})
-    })
-}
